c0b9c287e3885a16b7f26417a615ea298ca13988
Arthur Lapraye <arthurlapraye@yahoo.fr>
2015-06-21 15:43:15 +0200

    test fileg

---
+++ distancestest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import re
+from distances import *
+
+tabz=re.compile(r"\t")
+spaces=re.compile(r" ")
+with open("/home/arthur/programmes/python/projet/cognates+lat.txt") as f:
+	for line in f:
+		if line[0] == "#":
+			continue
+		else:
+			[es,it,_,pt] = tabz.split(line)
+			pt=pt[:-1]
+			print(pt+"\t"+es+"\t"+it+" "+str(avglev(es,pt))+"/"+str(avglev(es,it))+"/"+str(avglev(it,pt)))
+
+
